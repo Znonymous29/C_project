3e9d750da6a5e130edc6f6671d20535808925853
Znonymous29 <fly1029@yeah.net>
2018-07-15 13:05:10 +0800

    第七章

---
+++ 7/7-15.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+	  struct Base1{int x;};
+	  struct Base2{float y;};
+	  struct Derived:Base1,Base2{};
+	  
+	  int main(){
+		Derived *pd=new Derived;
+		pd->x=1;pd->y=2.0f;
+		Base2 *pb=pd;
+		cout<<pd->y<<" "<<pb->y<<endl;
+		delete pb;
+		return 0;
+		}
+
